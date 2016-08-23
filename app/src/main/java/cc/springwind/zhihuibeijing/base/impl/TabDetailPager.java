a7f9d9ccf7d76dad9fb7577429b4ec507e3abf0a
tloyd <hftloyd@gmail.com>
2016-08-23 16:36:43 +0800

    2016年8月23日 16:36:36

--- app/src/main/java/cc/springwind/zhihuibeijing/base/impl/TabDetailPager.java
+++ app/src/main/java/cc/springwind/zhihuibeijing/base/impl/TabDetailPager.java
@@ -5,10 +5,13 @@ import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.google.gson.Gson;
+import com.viewpagerindicator.CirclePageIndicator;
 
 import org.xutils.common.Callback;
 import org.xutils.http.RequestParams;
@@ -17,6 +20,8 @@ import org.xutils.x;
 
 import java.util.ArrayList;
 
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 import cc.springwind.zhihuibeijing.R;
 import cc.springwind.zhihuibeijing.base.BaseMenuDetailPager;
 import cc.springwind.zhihuibeijing.domain.NewsBean;
@@ -24,6 +29,7 @@ import cc.springwind.zhihuibeijing.domain.NewsTabBean;
 import cc.springwind.zhihuibeijing.global.Constants;
 import cc.springwind.zhihuibeijing.utils.CacheUtil;
 import cc.springwind.zhihuibeijing.utils.LogUtil;
+import cc.springwind.zhihuibeijing.view.TopNewsViewPager;
 
 /**
  * @author: HeFan
@@ -33,11 +39,16 @@ public class TabDetailPager extends BaseMenuDetailPager {
 
     private final NewsBean.NewsChild mNewsChild;
     private View view;
+    private View headerView; // ListView的头布局
     private String mUrl;
     private ListView lvNewsDpt;
-    private ViewPager vpTopDpt;
+    private TopNewsViewPager vpTopDpt;
+    private TextView tvTitleDpt;
     private NewsTabBean mbean;
     private ArrayList<NewsTabBean.TopNews> mTopnews;
+    private CirclePageIndicator indicatorTopnewsDpt;
+    private ArrayList<NewsTabBean.News> mNewsList;
+    private NewsAdapter mNewsAdapter;
 
     public TabDetailPager(Activity activity, NewsBean.NewsChild newsChild) {
         super(activity);
@@ -48,9 +59,18 @@ public class TabDetailPager extends BaseMenuDetailPager {
     @Override
     public View initView() {
         // 要给帧布局填充布局对象
+
         view = View.inflate(mActivity, R.layout.detail_pager_tab, null);
-        vpTopDpt = (ViewPager) view.findViewById(R.id.vp_top_dpt);
         lvNewsDpt = (ListView) view.findViewById(R.id.lv_news_dpt);
+        headerView = View.inflate(mActivity, R.layout.list_item_header, null);
+//        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 180);
+//        headerView.setLayoutParams(params);
+//        LayoutInflater lif = (LayoutInflater) mActivity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+//        headerView = lif.inflate(R.layout.list_item_header, lvNewsDpt, false);
+        vpTopDpt = (TopNewsViewPager) headerView.findViewById(R.id.vp_top_dpt);
+        tvTitleDpt = (TextView) headerView.findViewById(R.id.tv_title_dpt);
+        indicatorTopnewsDpt = (CirclePageIndicator) headerView.findViewById(R.id.indicator_topnews_dpt);
+        lvNewsDpt.addHeaderView(headerView); // 没数据的时候就要添加了
         return view;
     }
 
@@ -97,9 +117,100 @@ public class TabDetailPager extends BaseMenuDetailPager {
         mbean = gson.fromJson(json, NewsTabBean.class);
         mTopnews = mbean.data.topnews;
         vpTopDpt.setAdapter(new TopAdapter());
+        indicatorTopnewsDpt.setViewPager(vpTopDpt);
+        indicatorTopnewsDpt.setSnap(true); // 设置快照显示方式
+        indicatorTopnewsDpt.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                tvTitleDpt.setText(mbean.data.topnews.get(position).title);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+            }
+        });
+        tvTitleDpt.setText(mTopnews.get(0).title);
+        indicatorTopnewsDpt.onPageSelected(0); // 设置在页面初始化时,指示器指示第0个
+
+        mNewsList = mbean.data.news;
+        mNewsAdapter = new NewsAdapter();
+        lvNewsDpt.setAdapter(mNewsAdapter);
+
     }
 
+    class NewsAdapter extends BaseAdapter {
+        private final ImageOptions.Builder builder;
+
+        public NewsAdapter() {
+            builder = new ImageOptions.Builder();
+            builder.setFailureDrawableId(R.drawable.topnews_item_default)
+                    .setLoadingDrawableId(R.drawable.topnews_item_default);
+        }
+
+        @Override
+        public int getCount() {
+            return mNewsList.size();
+        }
+
+        @Override
+        public NewsTabBean.News getItem(int position) {
+            return mNewsList.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ViewHolder viewHolder;
+            if (convertView == null) {
+                convertView = View.inflate(mActivity, R.layout.list_item_news, null);
+                viewHolder = new ViewHolder(convertView);
+                convertView.setTag(viewHolder);
+            } else {
+                viewHolder = (ViewHolder) convertView.getTag();
+            }
+            NewsTabBean.News item = getItem(position);
+            viewHolder.tvNewsTitleLin.setText(item.title);
+            viewHolder.tvNewsDateLin.setText(item.pubdate);
+            x.image().bind(viewHolder.ivNewsLin, item.listimage, builder.build());
+            return convertView;
+        }
+
+
+    }
+
+    static class ViewHolder {
+        @InjectView(R.id.iv_news_lin)
+        ImageView ivNewsLin;
+        @InjectView(R.id.tv_news_title_lin)
+        TextView tvNewsTitleLin;
+        @InjectView(R.id.tv_news_date_lin)
+        TextView tvNewsDateLin;
+
+        ViewHolder(View view) {
+            ButterKnife.inject(this, view);
+        }
+    }
+
+    /**
+     * 头条滚动新文的适配器
+     */
     class TopAdapter extends PagerAdapter {
+
+        private final ImageOptions.Builder builder;
+
+        public TopAdapter() {
+            builder = new ImageOptions.Builder();
+            builder.setFailureDrawableId(R.drawable.topnews_item_default)
+                    .setLoadingDrawableId(R.drawable.topnews_item_default);
+        }
 
         @Override
         public int getCount() {
@@ -120,11 +231,7 @@ public class TabDetailPager extends BaseMenuDetailPager {
         public Object instantiateItem(ViewGroup container, int position) {
             ImageView view = new ImageView(mActivity);
             view.setScaleType(ImageView.ScaleType.FIT_XY);
-            ImageOptions.Builder builder = new ImageOptions.Builder();
-            builder.setFailureDrawableId(R.drawable.topnews_item_default)
-                    .setLoadingDrawableId(R.drawable.topnews_item_default);
             x.image().bind(view, mTopnews.get(position).topimage, builder.build());
-
             container.addView(view);
             return view;
         }
--- app/src/main/java/cc/springwind/zhihuibeijing/global/Constants.java
+++ app/src/main/java/cc/springwind/zhihuibeijing/global/Constants.java
@@ -7,6 +7,6 @@ public class Constants {
     // SharedPreference的KEY
     public static final String IS_FIRST_USE = "is_first_use"; // 是否第一次使用
     // 访问服务器的URL
-    public static final String SERVER_URL = "http://192.168.1.18:8080/zhbj";
+    public static final String SERVER_URL = "http://192.168.1.12:8080/zhbj";
     public static final String CATEGORIES_URL = SERVER_URL + "/categories.json"; // 新闻分类的URL
 }
